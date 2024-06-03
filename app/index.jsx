17482217ab411e2803df5e6aae2bff174a5f0f84
Rqkko <rakko26@gmail.com>
2024-06-03 16:33:41 +0700

    load assets

---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="font-pblack text-3xl">Aora!</Text>
+      <StatusBar style="auto" />
+      <Link href='profile' style={{ color: 'blue' }}>Go to Profile</Link>
+    </View>
+  );
+}
