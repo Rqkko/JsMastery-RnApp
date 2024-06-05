575e4d61cf288151e03b5f7007c3268c205904fe
Rqkko <rakko26@gmail.com>
2024-06-05 15:14:07 +0700

    edit home page
    Add all videos sections

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,5 +1,5 @@
-import { View, Text, FlatList, Image, RefreshControl } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, FlatList, Image, RefreshControl, Alert } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
 
@@ -7,23 +7,33 @@ import { images } from '../../constants'
 import SearchInput from '../../components/SearchInput'
 import Trending from '../../components/Trending'
 import EmptyState from '../../components/EmptyState'
+import { getAllPosts } from '../../lib/appwrite'
+import useAppwrite from '../../lib/useAppwrite'
+import VideoCard from '../../components/VideoCard'
 
 const Home = () => {
+  // Rename "data" to "posts"
+  const { data: posts, refetch } = useAppwrite(getAllPosts);
+
   const [refreshing, setRefreshing] = useState(false);
 
   const onRefresh = async () => {
     setRefreshing(true);
-    // Re call videos -> see if new videos appeare
+    await refetch();
     setRefreshing(false);
   }
 
   return (
     <SafeAreaView className="bg-primary h-full">
       <FlatList 
-      data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+      data={posts}
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
-          <Text className="text-3xl text-white">{item.id}</Text>
+          <>
+            <VideoCard 
+              video = {item}
+            /> 
+          </>
         )}
         ListHeaderComponent={() => (
           <View className="space-y-6 my-6 px-4">
---
+++ components/VideoCard.jsx
@@ -0,0 +1,70 @@
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { icons } from '../constants'
+
+const VideoCard = ({ video: { title, thumbnail, video, creator: { username, avatar } }}) => {
+  const [play, setPlay] = useState(false);
+
+  return (
+    <View className="flex-col items-center mb-14 px-4">
+      <View className="flex-row items-start gap-3">
+        <View className="flex-row flex-1 justify-center items-center">
+          <View className="justify-center items-center border-secondary p-0.5 border rounded-lg w-[46px] h-[46px] aasds">
+            <Image 
+              source={{ uri: avatar }} 
+              className="rounded-lg w-full h-full"
+              resizeMode='cover'
+            />
+          </View>
+
+          <View className="flex-1 justify-center gap-y-1 ml-3">
+            <Text 
+              className="font-psemibold text-sm text-white"
+              numberOfLines={1}
+            >
+              {title}
+            </Text>
+
+            <Text
+              className="text-=xs text-gray-100"
+              numberOfLines={1}
+            >
+              {username}
+            </Text>
+          </View>
+        </View>
+        
+        <View className="pt-2">
+          <Image 
+            source={icons.menu}
+            className="w-5 h-5"
+            resizeMode='contain'
+          />
+        </View>
+      </View>
+
+      {play ? (
+        <Text className="text-white">Playing</Text>
+      ) : (
+        <TouchableOpacity
+          className="relative justify-center items-center mt-3 rounded-xl w-full h-60"
+          activeOpacity={0.7}
+          onPress={() => setPlay(true)}
+        >
+          <Image 
+            source = {{ uri: thumbnail }}
+            className="mt-3 rounded-xl w-full h-full"
+            resizeMode="cover"
+          />
+          <Image 
+            source={icons.play}
+            className="absolute w-12 h-12"
+            resizeMode='contain'
+          />
+        </TouchableOpacity>
+      )}
+    </View>
+  )
+}
+
+export default VideoCard
