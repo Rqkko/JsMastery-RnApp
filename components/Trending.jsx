b275b4406acf38f09a34692fa0159aef4830a44f
Rqkko <rakko26@gmail.com>
2024-06-05 16:49:00 +0700

    edit Trending.jsx
    Still need to figure out why the video isn't playing (correct url of video, I log the url in the console)

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -1,19 +1,21 @@
 import { useState } from 'react';
-import { View, Text, FlatList, ImageBackground, TouchableOpacity } from 'react-native'
+import { View, Text, FlatList, ImageBackground, TouchableOpacity, Image } from 'react-native'
 import * as Animatable from 'react-native-animatable';
+import { icons } from '../constants';
+import { Video, ResizeMode } from 'expo-av';
 
 const zoomIn = {
   0: {
     scale: 0.9
   },
   1: {
-    scale: 1,
+    scale: 1.1,
   }
 }
 
 const zoomOut = {
   0: {
-    scale: 1
+    scale: 1.1
   },
   1: {
     scale: 0.9,
@@ -30,27 +32,55 @@ const TrendingItem = ({ activeItem, item }) => {
       duration={500}
     >
       {play ? (
-        <Text className="text-white">Playing</Text>
+        <>
+        <Video
+          source={{ uri: item.video }}
+          className="bg-white/10 mt-3 rounded-[35px] w-52 h-72"
+          resizeMode={ResizeMode.CONTAIN}
+          useNativeControls
+          shouldPlay
+          onPlaybackStatusUpdate={(status) => {
+            if (status.didJustFinish) {
+              setPlay(false);
+            }
+          }}
+        />
+        <Text className="text-white">PLAYING</Text>
+        </>
       ) : (
         <TouchableOpacity
           className="relative justify-center items-center"
           activeOpacity={0.7}
-          onPress={() => setPlay(true)}
+          onPress={() => {
+            setPlay(true)
+            console.log("Video url: " +item.video);
+          }}
         >
           <ImageBackground 
             source= {{ uri: item.thumbnail}}
             className="shadow-black/40 shadow-lg my-5 rounded-[35px] w-52 h-72 overflow-hidden"
             resizeMode='cover'
           />
+
+          <Image 
+            source={icons.play}
+            className="absolute w-12 h-12"
+            resizeMode='contain'
+          />
         </TouchableOpacity>
-      )
-    }
+      )}
     </Animatable.View>
   )
 }
 
 const Trending = ({ posts }) => {
-  const [activeItem, setActiveItem] = useState(posts[0]);
+  const [activeItem, setActiveItem] = useState(posts[1]);
+
+  function viewableItemsChanged({ viewableItems }) {
+    if (viewableItems.length > 0) {
+      setActiveItem(viewableItems[0].key)
+    }
+  }
 
   return (
     <FlatList
@@ -64,6 +94,11 @@ const Trending = ({ posts }) => {
           />
         </>
       )}
+      onViewableItemsChanged={viewableItemsChanged}
+      viewabilityConfig={{
+        itemVisiblePercentThreshold: 70
+      }}
+      contentOffset={{ x: 170 }}
       horizontal
     />
   )
